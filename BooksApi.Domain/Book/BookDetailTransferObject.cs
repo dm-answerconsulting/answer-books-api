3b2e41296d1eb0b5f19b21ec47ae0240954cf5dc
Daniel Morris <daniel.morris@answerconsulting.com>
2013-12-12 15:17:24 +0000

    Added id to the domain object which automatically mapped and was returned as expected.

--- BooksApi.Domain/Book/BookDetailTransferObject.cs
+++ BooksApi.Domain/Book/BookDetailTransferObject.cs
@@ -4,6 +4,7 @@ namespace BooksApi.Domain
 {
     public class BookDetailTransferObject
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Genre { get; set; }
         public DateTime PublishDate { get; set; }
--- BooksApi.Domain/Book/BookTransferObject.cs
+++ BooksApi.Domain/Book/BookTransferObject.cs
@@ -2,6 +2,7 @@
 {
     public class BookTransferObject
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public string Genre { get; set; }
